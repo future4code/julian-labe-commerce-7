21475480c1e2a3989959d1b9e6f91fddc76877aa
Leonardo <leonardolima2008@gmail.com>
2020-04-25 05:21:04 -0300

    atualizacao da funcao de ordenar

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,12 @@ const ProdutosHome = styled.div`
 text-align:center;
 margin-top: 10px;
 `
+const Imagem = styled.img`
+width: 200px;
+height: 250px;
+margin: 5px;
+border-radius: 5px;
+`
 const DivProdutos = styled.div`
 border:1px dotted #999;
 margin:10px;
@@ -23,6 +29,7 @@ margin-bottom: 5px;
 cursor:pointer;
 text-transform:uppercase;
 border-radius: 10px;
+outline: 0;
 `
 export class Home extends React.Component {
 
@@ -32,14 +39,14 @@ export class Home extends React.Component {
       const novaListaDeProdutos = this.props.listaDeProdutos.map((produto) => {
         if(!produto.carrinho){
           return <DivProdutos>
-            <img src={produto.imageUrl} />
+            <Imagem src={produto.imageUrl} />
             <p>{produto.name}</p>
             <p>Valor: R$ {produto.value}</p>
             <Button onClick={() => this.props.adicionarProduto(produto.id, produto.name, produto.value, produto.imageUrl)}>Adicionar Carrinho</Button>
           </DivProdutos>
         }
         return <DivProdutos>
-            <img src={produto.imageUrl} />
+            <Imagem src={produto.imageUrl} />
             <p>{produto.name}</p>
             <p>Valor: R$ {produto.value}</p>
             <Button disabled>Já está no carrinho</Button>
